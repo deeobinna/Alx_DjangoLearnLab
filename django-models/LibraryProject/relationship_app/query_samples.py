f7b5d92449b54369a524ebfb9cc3a0d1070de916
Desmond Obinna <talk2desmond91@gmail.com>
2025-07-19 15:07:46 +0100

    django models, Implementing OneToMany and ManyToMany Relationship

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,12 +1,11 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-def create_sample_data():
-    #query all books by a specific author
-    author = Author.objects.get(name='J.K. Rowling')
+#list all books in the library 
+books = Book.objects.all()
+
+#query all books by a specific author
+author = Author.objects.get(name='J.K. Rowling')
     
-    #get all books in the library   
-    books = Library.objects.get(name = "library_name").books.all()
-    
-    #retrieve the librarian for a library
-    librarian = Librarian.objects.get(name='City Library')
+#retrieve the librarian for a library
+librarian = Librarian.objects.get(name='City Library')
     
