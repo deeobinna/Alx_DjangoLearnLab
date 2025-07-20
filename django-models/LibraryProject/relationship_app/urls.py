0e3bd18a49d6ffca8ab340a62ec37bd1afcceb88
Desmond Obinna <talk2desmond91@gmail.com>
2025-07-20 11:47:09 +0100

    django models, Implementing OneToMany and ManyToMany Relationship

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('login/',auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
-    path('register/', auth_views.RegisterView.as_view(template_name='relationship_app/register.html'), name='register'),
+    path('register/', views.RegisterView.as_view(template_name='relationship_app/register.html'), name='register'),
 ]
