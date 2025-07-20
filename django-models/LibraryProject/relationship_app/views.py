55f9f62979b2140a2a757d396de0818c6fd7d682
Desmond Obinna <talk2desmond91@gmail.com>
2025-07-20 11:11:20 +0100

    django models, Implementing OneToMany and ManyToMany Relationship

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,9 +7,14 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
     
-#Create a class-based view in relationship_app/views.py that displays details for a specific library, listing all books available in that library.
-
+    
+#class based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()
+        return context
