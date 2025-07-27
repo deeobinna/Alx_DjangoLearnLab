c5ea034e9ff7b31fdde905877f88ffc91c60bc30
Desmond Obinna <talk2desmond91@gmail.com>
2025-07-27 13:41:17 +0100

    Custom User Models

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+# Register the custom user model with the UserAdmin
 # Register your models here.
 
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ('email', 'username', 'date_of_birth', 'is_staff')
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+admin.site.register(CustomUser, CustomUserAdmin)
 class BookAdmin(admin.ModelAdmin):
     list_filter = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,4 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self,date_of_birth, profile_photo):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email,date_of_birth)
+        user.set_password(None)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,date_of_birth, profile_photo):
+        user = self.create_user(date_of_birth, profile_photo)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+    
+    
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+    date_of_birth = models.DateField(null=True, blank=True) 
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    objects = CustomUserManager()   
+#create customUserManager
+
+
+
+
+
 
 # Create your models here.
 class Book(models.Model):
